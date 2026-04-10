491d5d47bb52eb5aeb1b119cec2397f285cc2a07
Pyshechkaaa <no4321321@gmail.com>
2026-04-11 01:10:18 +0300

    Update main_bot.py

--- main_bot.py
+++ main_bot.py
@@ -1,6 +1,8 @@
 import asyncio
 import os
+
 from aiogram import Bot, Dispatcher, types
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 
 TOKEN = os.getenv("BOT_TOKEN")
 
@@ -10,13 +12,31 @@ if not TOKEN:
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+# 👉 ссылка на твой Mini App (Render)
+WEBAPP_URL = "https://telegram-app-dp2l.onrender.com"
+
 @dp.message()
 async def start(message: types.Message):
-    await message.answer("бот работает 🚀")
+
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+        [
+            InlineKeyboardButton(
+                text="🚀 Открыть Mini App",
+                web_app=WebAppInfo(url=WEBAPP_URL)
+            )
+        ]
+    ])
+
+    await message.answer(
+        "Привет 👋\nНажми кнопку чтобы открыть приложение:",
+        reply_markup=keyboard
+    )
+
 
 async def main():
     print("BOT STARTED")
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
