0fad507379922a3eb59e7837e0b76d2a7a353f69
Pyshechkaaa <no4321321@gmail.com>
2026-04-11 00:47:55 +0300

    Update main_bot.py

--- main_bot.py
+++ main_bot.py
@@ -4,6 +4,9 @@ from aiogram import Bot, Dispatcher, types
 
 TOKEN = os.getenv("8667775358:AAEGTOTAoEmZsuQLE54eA60OQXqTCIbLTTM")
 
+if not TOKEN:
+    raise Exception("8667775358:AAEGTOTAoEmZsuQLE54eA60OQXqTCIbLTTM is missing")
+
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
@@ -12,6 +15,7 @@ async def start(message: types.Message):
     await message.answer("бот работает 🚀")
 
 async def main():
+    print("BOT STARTED")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
